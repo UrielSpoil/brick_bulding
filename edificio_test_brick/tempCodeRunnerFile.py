18738ffa13907a30ff1142efabf542ed49bbb4ac
UrielSpoil <77375070+UrielSpoil@users.noreply.github.com>
2021-03-25 15:53:23 -0700

    Delete edificio_test_brick directory

--- edificio_test_brick/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-floor1 = g.query("""SELECT DISTINCT ?sensor WHERE 
-    {
-       :floor1 (brick:hasPart|^brick:isPartOf|brick:hasPoint|^brick:isPointOf|brick:isLocationOf|^brick:hasLocation)+ ?sensor .  ?sensor a/rdfs:subClassOf* brick:Sensor .
-    } """)
-
-for row in floor1:
-    print(row)
